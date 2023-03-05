d1b221381e7b6bd2dbd7b60ed51f657be6f0d1b3
mahmoodharoon <horounmahmood@gmail.com>
2023-03-05 20:10:21 +0200

    customize Material Request

---
+++ erpnext_gsg/erpnext_gsg/event/material_request_event.py
@@ -0,0 +1,25 @@
+import frappe
+@frappe.whitelist()
+def create_stock_entry(doc, event):
+    print("*"*100)
+    print(doc.items[0].item_code)
+    
+    if(doc.material_request_type != "Material Issue"):
+        return
+        
+    from erpnext.stock.doctype.material_request.material_request import make_stock_entry
+    stentry = make_stock_entry(doc.name)
+    stentry.insert()
+    stentry.submit()
+    #     stock_entry_doc = frappe.new_doc("Stock Entry")
+    #     stock_entry_doc.stock_entry_type = doc.material_request_type
+    #     stock_entry_doc.from_warehouse = doc.set_from_warehouse
+    #     for item in doc.get("items"):
+    #         stock_entry_doc.append("items",{
+    #             "s_warehouse":item.from_warehouse, 
+    #             "t_warehouse": item.warehouse,
+    #             "qty": item.qty,
+    #             "item_code": item.item_code
+    #         })
+
+    #     stock_entry_doc.insert()
