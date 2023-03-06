05e0079b208f79c5c386b0ca0c46f9f67d6a6cb3
mahmoodharoon <horounmahmood@gmail.com>
2023-03-06 21:58:22 +0200

    Update Employee Excuse Application

--- erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.js
+++ erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.js
@@ -5,4 +5,27 @@ frappe.ui.form.on('Employee Excuse Application', {
 	// refresh: function(frm) {
 
 	// }
+	from_time:function(frm){
+		frm.trigger('get_hours');
+	},
+	to_time:function(frm){
+		frm.trigger('get_hours');
+	},
+	get_hours: function(frm){
+		if((!frm.doc.from_time | !frm.doc.to_time )){
+			return;
+		}
+		frappe.call({
+			method:"erpnext_gsg.erpnext_gsg.doctype.employee_excuse_application.employee_excuse_application.clac_hours",
+			args:{
+				from_time:frm.doc.from_time,
+				to_time:frm.doc.to_time	
+			},
+			callback: (r)=>{
+				frm.doc.hours = r.message
+				frm.refresh()
+			}
+
+		})
+	}
 });
--- erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.py
+++ erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.py
@@ -11,30 +11,36 @@ class EmployeeExcuseApplication(Document):
 		self.calc_hours()
 
 	def calc_hours(self):
-		print("*"*100)
 		diff = time_diff_in_hours(self.to_time, self.from_time)
-		
 
 		excuse_hours_alowed = frappe.db.get_list('Department',
 			filters={
 				'department_name': self.department
 			},
 			fields=['excuse_hours_alowed'],
-				)
-		print(excuse_hours_alowed[0].excuse_hours_alowed)
+			)
 
 		excuse_hours = frappe.db.get_list('Employee Excuse Application',
 			filters={
 				'employee': self.employee
-			},
+				},
 			fields=['hours'],
-				)
+			)
+		
 		sum_excuse_hours = 0
 		for i in excuse_hours:
 			sum_excuse_hours += i.hours
-		
-		if (sum_excuse_hours + diff < excuse_hours_alowed[0].excuse_hours_alowed):
-			self.hours = diff
-		else:
-			frappe.throw("You don't have enough time to excuse.")
+		if(self.to_time > self.from_time):
+			if (sum_excuse_hours + diff < excuse_hours_alowed[0].excuse_hours_alowed):
+				self.hours = diff
+			else:
+				frappe.throw("You don't have enough time to excuse.")
 
+		else:
+			frappe.throw("Can not put To Time before From time.")
+	
+			
+@frappe.whitelist()
+def clac_hours(from_time, to_time):
+	if from_time and to_time:
+		return time_diff_in_hours(to_time, from_time) +1 
