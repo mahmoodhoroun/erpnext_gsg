f1ac175effe87d9df58b89900a89d1863ad9bada
mahmoodharoon <horounmahmood@gmail.com>
2023-03-06 17:23:00 +0200

    Update Employee Excuse Application

--- erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.py
+++ erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.py
@@ -1,8 +1,40 @@
 # Copyright (c) 2023, mahmoud haroun and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+from frappe.utils import time_diff_in_hours
+
 class EmployeeExcuseApplication(Document):
-	pass
+	def validate(self):
+		self.calc_hours()
+
+	def calc_hours(self):
+		print("*"*100)
+		diff = time_diff_in_hours(self.to_time, self.from_time)
+		
+
+		excuse_hours_alowed = frappe.db.get_list('Department',
+			filters={
+				'department_name': self.department
+			},
+			fields=['excuse_hours_alowed'],
+				)
+		print(excuse_hours_alowed[0].excuse_hours_alowed)
+
+		excuse_hours = frappe.db.get_list('Employee Excuse Application',
+			filters={
+				'employee': self.employee
+			},
+			fields=['hours'],
+				)
+		sum_excuse_hours = 0
+		for i in excuse_hours:
+			sum_excuse_hours += i.hours
+		
+		if (sum_excuse_hours + diff < excuse_hours_alowed[0].excuse_hours_alowed):
+			self.hours = diff
+		else:
+			frappe.throw("You don't have enough time to excuse.")
+
