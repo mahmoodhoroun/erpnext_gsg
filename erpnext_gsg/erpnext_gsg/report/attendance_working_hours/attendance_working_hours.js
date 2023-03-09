cf7f3f36a4f1d6d2a709d56e36ec2d319e01f297
mahmoodharoon <horounmahmood@gmail.com>
2023-03-09 15:11:14 +0200

    update attendance working hours report

--- erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.js
+++ erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.js
@@ -38,28 +38,3 @@ frappe.query_reports["Attendance Working Hours"] = {
 
 	]
 };
-// frappe.templates["my_template_name"] = " \
-//     <div class='form-group'> \
-//         <label class='control-label' for='my_field_name'>My Field Label</label> \
-//         <div class='controls'> \
-//             <input type='text' class='form-control' id='my_field_name' name='my_field_name' data-fieldtype='Data' /> \
-//         </div> \
-//     </div>";
-	
-
-frappe.ui.form.on('Attendance Working Hours', {
-    refresh: function(frm) {
-        // Add HTML attributes to a link in the report
-		print("*"*100)
-        frm.fields_dict['name'].get_query = function(doc) {
-            return {
-                filters: {
-                    'doctype': 'Attendance',
-                    'fieldname': 'name',
-                    'link': '<a href="/app/attendance/HR-ATT-2023-00001" data-doctype="Attendance" data-name="HR-ATT-2023-00001" data-value="HR-ATT-2023-00001" target="_blank>HR-ATT-2023-00001</a>'
-					
-                }
-            };
-        };
-    }
-});
